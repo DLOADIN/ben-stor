d6ac281ac83588e95e208d1a2ffda8f78b4601b3
DLOADIN <m.david@alustudent.com>
2024-12-28 05:24:58 +0200

    ben store

--- src/app/components/layout/Bsservices.tsx
+++ src/app/components/layout/Bsservices.tsx
@@ -41,44 +41,45 @@ export default function Projects (){
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
     >
-      {services.map((service,index)=>(
-         <div className="grid md:grid-cols-2 gap-8 items-center py-10" key={index}>
-          <div className="order-2 md:order-1">
-            <motion.h3 
-              className="text-2xl font-semibold text-black mb-4"
-              initial={{ opacity: 0, x: -20 }}
-              whileInView={{ opacity: 1, x: 0 }}
-              transition={{ delay: 0.2 }}
-            >
-              {service.title}
-            </motion.h3>
-            <motion.p 
-              className="text-gray-400"
-              initial={{ opacity: 0, x: -20 }}
-              whileInView={{ opacity: 1, x: 0 }}
-              transition={{ delay: 0.3 }}
-            >
-              {service.description}
-            </motion.p>
-            <motion.div
-              className="mt-6"
-              initial={{ opacity: 0, x: -20 }}
-              whileInView={{ opacity: 1, x: 0 }}
-              transition={{ delay: 0.4 }}
-            >
-            </motion.div>
-          </div>
-          <div className="order-1 md:order-2 overflow-hidden rounded-lg w-full h-full">
-          <motion.img
-            src={typeof service.image === 'string' ? service.image : service.image.src}
-            alt={service.title}
-            className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105"
-            initial={{ opacity: 0, scale: 0.95 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.5 }}
-          />
-            </div>
-        </div>))}
+     {services.map((service, index) => (
+  <div className="grid md:grid-cols-2 gap-8 items-center py-10" key={index}>
+    <div className={`${index % 2 === 0 ? 'order-2 md:order-1' : 'order-2 md:order-2'}`}>
+      <motion.h3 
+        className="text-2xl font-semibold text-black mb-4"
+        initial={{ opacity: 0, x: -20 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ delay: 0.2 }}
+      >
+        {service.title}
+      </motion.h3>
+      <motion.p 
+        className="text-gray-400"
+        initial={{ opacity: 0, x: -20 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ delay: 0.3 }}
+      >
+        {service.description}
+      </motion.p>
+      <motion.div
+        className="mt-6"
+        initial={{ opacity: 0, x: -20 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ delay: 0.4 }}
+      >
+      </motion.div>
+    </div>
+    <div className={`${index % 2 === 0 ? 'order-1 md:order-2' : 'order-1 md:order-1'} overflow-hidden rounded-lg w-full h-full`}>
+      <motion.img
+        src={typeof service.image === 'string' ? service.image : service.image.src}
+        alt={service.title}
+        className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105"
+        initial={{ opacity: 0, scale: 0.95 }}
+        whileInView={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
+      />
+    </div>
+  </div>
+))}
     </motion.div>
       </motion.div>
     </div>
