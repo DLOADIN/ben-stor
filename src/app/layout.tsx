1af337394aa246163a8634576831ca2c27d568d3
Lemanzi David <166613293+DLOADIN@users.noreply.github.com>
2025-01-04 16:04:54 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,34 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-
 export const metadata: Metadata = {
-  title: "BS BRAND",
+  metadataBase: new URL("https://benstores.vercel.app/"),
+  title: {
+    default: "BS BRAND",
+    template: "%s | BS BRAND",
+  },
   description: "BEN STORES AND BS BRAND",
-  openGraph:{
+  openGraph: {
     title: "BS BRAND",
-    type:"website",
-    locale:"en_US",
-    siteName:"BSBRAND"
+    type: "website",
+    locale: "en_US",
+    siteName: "BSBRAND",
   },
 };
 
-export default function RootLayout({children,}: Readonly<{ children: React.ReactNode;}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-  <body
-    style={{
-      backgroundImage: "linear-gradient(linear-gradient(to top, #dfe9f3 0%, white 100%)",
-      backgroundColor: "black",
-      color: "black",
-    }}
-    className="antialiased"
-  >
-    {children}
-  </body>
-</html>
-);
+      <body
+        style={{
+          backgroundImage: "linear-gradient(to top, #dfe9f3 0%, white 100%)",
+          backgroundColor: "black",
+          color: "black",
+        }}
+        className="antialiased"
+      >
+        {children}
+      </body>
+    </html>
+  );
 }
