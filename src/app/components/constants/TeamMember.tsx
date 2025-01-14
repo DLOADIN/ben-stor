aa79d1c43f443ffb828ed776d61baf24b755ae76
DLOADIN <m.david@alustudent.com>
2025-01-14 19:25:46 +0200

    bn stores

--- src/app/components/constants/TeamMember.tsx
+++ src/app/components/constants/TeamMember.tsx
@@ -20,6 +20,80 @@ import image20 from '@/PICS/DGT_677.jpg';
 import image21 from '@/PICS/TAKI9589.jpg';
 import image22 from '@/PICS/TAKI9596.jpg';
 import image23 from '@/PICS/TAKI9599.jpg';
+import image24 from '@/PICS/mycera.jpg';
+import image25 from '@/PICS/TAKI9586.jpg';
+import editionimg1 from '@/assets/EDITION3/NEWONES/PIC1.jpg'
+import editionimg2 from '@/assets/EDITION3/NEWONES/PIC2.jpg'
+import editionimg3 from '@/assets/EDITION3/NEWONES/chica.jpg'
+import editionimg4 from '@/assets/EDITION3/NEWONES/DSC09560.jpg';
+import editionimg5 from '@/assets/EDITION3/DSC09562.jpg';
+import editionimg6 from '@/assets/EDITION3/DSC09564.jpg';
+import editionimg7 from '@/assets/EDITION3/DSC09568.jpg';
+import editionimg8 from '@/assets/EDITION3/DSC09571.jpg';
+import editionimg10 from '@/assets/EDITION3/DSC09582.jpg';
+import editionimg11 from '@/assets/EDITION3/DSC09601.jpg';
+import editionimg12 from '@/assets/EDITION3/DSC09602.jpg';
+import editionimg13 from '@/assets/EDITION3/DSC09603.jpg';
+import editionimg14 from '@/assets/EDITION3/DSC09606.jpg';
+import editionimg16 from '@/assets/EDITION3/DSC09622.jpg';
+import editionimg17 from '@/assets/EDITION3/DSC09624.jpg';
+import editionimg18 from '@/assets/EDITION3/DSC09633.jpg';
+import editionimg19 from '@/assets/EDITION3/DSC09640.jpg';
+import editionimg20 from '@/assets/EDITION3/DSC09654.jpg';
+import editionimg21 from '@/assets/EDITION3/DSC09658.jpg';
+import editionimg22 from '@/assets/EDITION3/DSC09660.jpg';
+import editionimg23 from '@/assets/EDITION3/DSC09669.jpg';
+import editionimg24 from '@/assets/EDITION3/DSC09675.jpg';
+import editionimg25 from '@/assets/EDITION3/DSC09681.jpg';
+import editionimg26 from '@/assets/EDITION3/DSC09683.jpg';
+import editionimg27 from '@/assets/EDITION3/DSC09702.jpg';
+import editionimg28 from '@/assets/EDITION3/DSC09703.jpg';
+import editionimg29 from '@/assets/EDITION3/DSC09704.jpg';
+import editionimg30 from '@/assets/EDITION3/DSC09707.jpg';
+import editionimg31 from '@/assets/EDITION3/DSC09708.jpg';
+import editionimg32 from '@/assets/EDITION3/DSC09717.jpg';
+import editionimg33 from '@/assets/EDITION3/DSC09719.jpg';
+import editionimg34 from '@/assets/EDITION3/DSC09724.jpg';
+import editionimg35 from '@/assets/EDITION3/DSC09728.jpg';
+import editionimg36 from '@/assets/EDITION3/DSC09729.jpg';
+import editionimg37 from '@/assets/EDITION3/DSC09732.jpg';
+import editionimg38 from '@/assets/EDITION3/DSC09737.jpg';
+import editionimg39 from '@/assets/EDITION3/DSC09743.jpg';
+import editionimg40 from '@/assets/EDITION3/DSC09746.jpg';
+import editionimg41 from '@/assets/EDITION3/DSC09757.jpg';
+import editionimg42 from '@/assets/EDITION3/DSC09762.jpg';
+import editionimg43 from '@/assets/EDITION3/DSC09763.jpg';
+import editionimg44 from '@/assets/EDITION3/DSC09770.jpg';
+import editionimg45 from '@/assets/EDITION3/DSC09776.jpg';
+import editionimg46 from '@/assets/EDITION3/DSC09784.jpg';
+import editionimg47 from '@/assets/EDITION3/DSC09786.jpg';
+import editionimg48 from '@/assets/EDITION3/DSC09788.jpg';
+import editionimg49 from '@/assets/EDITION3/DSC09792.jpg';
+import editionimg50 from '@/assets/EDITION3/DSC09796.jpg';
+import editionimg51 from '@/assets/EDITION3/DSC09798.jpg';
+import editionimg52 from '@/assets/EDITION3/DSC09831.jpg';
+import editionimg53 from '@/assets/EDITION3/DSC09836.jpg';
+import editionimg54 from '@/assets/EDITION3/DSC09840.jpg';
+import editionimg55 from '@/assets/EDITION3/DSC09840(1).jpg';
+import editionimg56 from '@/assets/EDITION3/DSC09843.jpg';
+import editionimg57 from '@/assets/EDITION3/DSC09845.jpg';
+import editionimg58 from '@/assets/EDITION3/DSC09846.jpg';
+import editionimg59 from '@/assets/EDITION3/DSC09850.jpg';
+import editionimg60 from '@/assets/EDITION3/DSC09851.jpg';
+import editionimg61 from '@/assets/EDITION3/DSC09859.jpg';
+import editionimg62 from '@/assets/EDITION3/DSC09861.jpg';
+import editionimg63 from '@/assets/EDITION3/DSC09891.jpg';
+import editionimg64 from '@/assets/EDITION3/DSC09918.jpg';
+import editionimg65 from '@/assets/EDITION3/DSC09922.jpg';
+import editionimg66 from '@/assets/EDITION3/DSC09924.jpg';
+import editionimg67 from '@/assets/EDITION3/DSC09931.jpg';
+import editionimg68 from '@/assets/EDITION3/DSC09932.jpg';
+import editionimg69 from '@/assets/EDITION3/DSC09934.jpg';
+import editionimg70 from '@/assets/EDITION3/DSC09939.jpg';
+import editionimg71 from '@/assets/EDITION3/DSC09953.jpg';
+import editionimg72 from '@/assets/EDITION3/DSC09971.jpg';
+import editionimg73 from '@/assets/EDITION3/DSC09972.jpg';
+import editionimg74 from '@/assets/EDITION3/DSC09980.jpg';
 
 export interface TeamMember {
   name: string;
@@ -55,6 +129,10 @@ export const teamMembers: TeamMember[] = [
       {
         role: "Unisex",
         image: image21
+      },
+      {
+        role: "Unisex",
+        image: image25
       }
     ]
   },
@@ -89,6 +167,257 @@ export const teamMembers: TeamMember[] = [
     ]
   },
   {
+    name: "BS PETITE T-SHIRTS",
+    role: "Female",
+    image: editionimg8,
+    variants:[
+        {
+          role: "Female",
+          image: editionimg6
+        },
+        {
+          role: "Female",
+          image: editionimg7
+        },
+        {
+          role: "Female",
+          image: editionimg5
+        },
+        {
+          role: "Female",
+          image: editionimg10
+        },
+        {
+          role: "Female",
+          image: editionimg11
+        },
+        {
+          role: "Female",
+          image: editionimg12
+        },
+        {
+          role: "Female",
+          image: editionimg13
+        },
+        {
+          role: "Female",
+          image: editionimg14
+        },
+        {
+          role: "Female",
+          image: editionimg16
+        },
+    ]
+  },
+  {
+    name: "BS ELEGANT DRESSES",
+    role: "Female",
+    image: editionimg20,
+    variants:[
+      {
+        role: "Female",
+        image: editionimg17
+      },
+      {
+        role: "Female",
+        image: editionimg18
+      },
+      {
+        role: "Female",
+        image: editionimg19
+      },
+      {
+        role: "Female",
+        image: editionimg21
+      },
+      {
+        role: "Female",
+        image: editionimg22
+      },
+      {
+        role: "Female",
+        image: editionimg27
+      },
+      {
+        role: "Female",
+        image: editionimg28
+      },
+      {
+        role: "Female",
+        image: editionimg29
+      },
+      {
+        role: "Female",
+        image: editionimg30
+      },
+      {
+        role: "Female",
+        image: editionimg31
+      },
+  ]
+  },
+  {
+    name: "BS SLEEVELESS MADE IN RWANDA SHIRTS",
+    role: "Male",
+    image: editionimg32,
+    variants: [
+      {
+        role: "Male",
+        image: editionimg33
+      },
+      {
+        role: "Male",
+        image: editionimg34
+      },
+      {
+        role: "Male",
+        image: editionimg35
+      },
+      {
+        role: "Male",
+        image: editionimg44
+      },
+      {
+        role: "Male",
+        image: editionimg45
+      },
+      {
+        role: "Male",
+        image: editionimg46
+      },
+      {
+        role: "Male",
+        image: editionimg47
+      },
+      {
+        role: "Male",
+        image: editionimg48
+      },
+      {
+        role: "Male",
+        image: editionimg49
+      },
+      {
+        role: "Male",
+        image: editionimg50
+      },
+      {
+        role: "Male",
+        image: editionimg51
+      },
+
+    ]
+  },
+  {
+    name: "BS SUITS",
+    role: "Male",
+    image: editionimg32,
+    variants: [
+      {
+        role: "Male",
+        image: editionimg52
+      },
+      {
+        role: "Male",
+        image: editionimg53
+      },
+      {
+        role: "Male",
+        image: editionimg54
+      },
+      {
+        role: "Male",
+        image: editionimg55
+      },
+      {
+        role: "Male",
+        image: editionimg56
+      },
+      {
+        role: "Male",
+        image: editionimg57
+      },
+      {
+        role: "Male",
+        image: editionimg58
+      },
+    ]},
+  {
+    name: "BS VESTS",
+    role: "Unisex",
+    image: editionimg23,
+    variants: [
+      {
+        role: "Unisex",
+        image: editionimg66
+      },
+      {
+        role: "Unisex",
+        image: editionimg24
+      },
+      {
+        role: "Unisex",
+        image: editionimg25
+      },
+      {
+        role: "Unisex",
+        image: editionimg26
+      },
+      {
+        role: "Unisex",
+        image: editionimg36
+      },
+      {
+        role: "Unisex",
+        image: editionimg37
+      },
+      {
+        role: "Unisex",
+        image: editionimg38
+      },
+      {
+        role: "Unisex",
+        image: editionimg39
+      },
+      {
+        role: "Unisex",
+        image: editionimg40
+      },
+      {
+        role: "Unisex",
+        image: editionimg67
+      },
+      {
+        role: "Unisex",
+        image: editionimg68
+      },
+      {
+        role: "Unisex",
+        image: editionimg69
+      },
+      {
+        role: "Unisex",
+        image: editionimg70
+      },
+      {
+        role: "Unisex",
+        image: editionimg71
+      },
+      {
+        role: "Unisex",
+        image: editionimg72
+      },
+      {
+        role: "Unisex",
+        image: editionimg73
+      },
+      {
+        role: "Unisex",
+        image: editionimg74
+      },
+    ]
+  },
+  {
     name: "BS SHORT SLEEVES U NECK",
     role: "Women",
     image: image8,
@@ -96,7 +425,21 @@ export const teamMembers: TeamMember[] = [
       {
         role: "Women",
         image: image11
-      }
+      },
+      {
+        role: "Women",
+        image: editionimg1
+      },{
+        role: "Women",
+        image: editionimg2
+      },{
+        role: "Women",
+        image: editionimg3
+      },
+      {
+        role: "Women",
+        image: editionimg4
+      },
     ]
   },
   {
@@ -107,7 +450,19 @@ export const teamMembers: TeamMember[] = [
       {
         role: "Male",
         image: image22
-      }
+      },
+      {
+        role: "Male",
+        image: editionimg41
+      },
+      {
+        role: "Male",
+        image: editionimg42
+      },
+      {
+        role: "Male",
+        image: editionimg43
+      },
     ]
   },
   {
@@ -118,6 +473,10 @@ export const teamMembers: TeamMember[] = [
       {
         role: "Female",
         image: image20
+      },
+      {
+        role: "Female",
+        image: image24
       }
     ]
   },
